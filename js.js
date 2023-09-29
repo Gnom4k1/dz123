261a509fbe5ed6d8eb9ed27d74d1552e6e67b3a6
Rayan_Osling <100954683+Gnom4k1@users.noreply.github.com>
2023-09-29 15:50:22 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,124 @@
+// let a = 1
+// if (a == 0) {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let a = -3
+// if (a > 0) {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let a = -3
+// if (a < 0) {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let a = -3
+// if (a >= 0) {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let a = -3
+// if (a <= 0) {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let a = -3
+// if (a != 0) {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let a = "test"
+// if (a == "test") {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let a = "1"
+// if (a == "1") {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let test = true
+// if (test == true) {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let test = true
+// if (test != true) {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let a = 2
+// if (a > 0 && a < 5) {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let a = 2
+// if (a == 0 || a == 2) {
+//     a += 7
+// }else{
+//     a = a/10
+// }
+
+// let a = 2
+// let b = 5
+// if (a <= 1 || b >= 3) {
+//     console.log(a+b)
+// }else{
+//     console.log(a-b)
+// }
+
+// let a = 2
+// let b = 5
+// if (a > 2 || a < 11 && b >= 6 || b < 11) {
+//     console.log("верно")
+// }else{
+//     console.log("неверно")
+// }
+
+// let day = 16
+// if (day <= 15){
+//     console.log("в первую")
+// }else if( day > 15){
+//     console.log("во вторую")
+// }
+
+// let month = 7
+// if (month > 2 || month < 6){
+//     console.log("Весна")
+// else if (month > 5 || month < 9){
+//     console.log("Лето")
+// else if (month > 8 || month < 12){
+//     console.log("Осень")
+// else if (month == 11 && month == 12 && month == 1){
+//     console.log("Зима")
+// }
+
+// let str = "Hello,"
+// let str1 = "World!"
+// alert(str + str1)
+
+//let num = prompt("Число")
+//alert(num ** 2)
